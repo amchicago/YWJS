205a629db28e43b87d733592f6b0cf149b17b2cb
WQQHome <250979411@qq.com>
2017-08-14 10:10:42 +0800

    1111

---
+++ JSCenter/JSCenter.Model/Tongji.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSCenter.Model
+{
+    /// <summary>
+    /// 统计信息
+    /// </summary>
+    public class Tongji
+    {
+        public string GroupName { get; set; }
+
+        /// <summary>
+        /// 汤剂含量
+        /// </summary>
+        public double TJHL { get; set; }
+
+        /// <summary>
+        /// 饮片含量
+        /// </summary>
+        public double YPHL { get; set; }
+
+        /// <summary>
+        /// 转移率
+        /// </summary>
+        public double ZYLv { get; set; }
+
+    }
+}
--- JSCenter/JSCenter.Win/SysManager.cs
+++ JSCenter/JSCenter.Win/SysManager.cs
@@ -9,7 +9,7 @@ namespace JSCenter.Win
         #region 读写配置
         private const string sysFile = "jsconfig.xml";
 
-        private  static string _GetPath()
+        private static string _GetPath()
         {
             return string.Format(@"{0}\{1}", AppDomain.CurrentDomain.BaseDirectory, sysFile);
         }
@@ -49,12 +49,11 @@ namespace JSCenter.Win
         /// <summary>
         /// 去读配置项重新计算结果,更新数据
         /// </summary>
-        public static  void ReCalculation(string projectId)
+        public static void ReCalculation(string projectId)
         {
             var list = DAL.CommonDAL.GetProjectItemList(projectId);
-            var fucelist = list.Where(s => s.IsFuCe == "True").ToList();
-            var normallist = list.Where(s => s.IsFuCe == "False").ToList();
-
+            
+            var tongjiList = new List<Model.Tongji>();
             #region 加载配置
             Model.SysConfig config = ReadConfig();
             if (config == null || string.IsNullOrEmpty(config.HanLiang))
@@ -62,60 +61,108 @@ namespace JSCenter.Win
                 return;
             }
             #endregion
-            
-            # region 计算含量
-            Action<Model.DrugProjectItem> countHL=(Model.DrugProjectItem s) => 
-            {
-                try
-                {
-                    var dic = new Dictionary<string, FloatingPoint>()
-                    {
+
+            #region 计算含量
+            Action<Model.DrugProjectItem> countHL = (Model.DrugProjectItem s) =>
+              {
+                  try
+                  {
+                      var dic = new Dictionary<string, FloatingPoint>()
+                      {
                          {"平均供试峰面积",float.Parse(s.PJSFMJ)},
                          {"供试称样量",float.Parse(s.GSCYL)},
                          {"稀释倍数",float.Parse(s.XSBS)},
                          {"对照浓度",float.Parse(s.DZLD)},
                          {"对照峰面积",float.Parse(s.DZFMJ)}
-                    };
-                    FloatingPoint value = Evaluate.Evaluate(dic, Infix.ParseOrUndefined(config.HanLiang));
-                    s.HL = Math.Round(value.RealValue, config.HanLiangPoint, MidpointRounding.AwayFromZero).ToString();
-                }
-                catch (Exception)
-                {
+                      };
+                      FloatingPoint value = Evaluate.Evaluate(dic, Infix.ParseOrUndefined(config.HanLiang));
+                      s.HL = Math.Round(value.RealValue, config.HanLiangPoint, MidpointRounding.AwayFromZero).ToString();
+                  }
+                  catch (Exception)
+                  {
 
-                }
-            };
-            normallist.ForEach(s => countHL(s));
-            fucelist.ForEach(s => countHL(s));
+                  }
+              };
             #endregion
 
             #region 计算平均含量和方差
-            
-            foreach ( var item in normallist.GroupBy(s=>s.CodeNum1))
+
+            var yplist = list.Where(s => s.type == Model.DrugType.饮片.ToString());
+            var tjlist = list.Where(s => s.type == Model.DrugType.汤剂.ToString());
+
+            Action<string ,double,bool> addTongji = (string key, double PJHL,bool isYp) =>
+              {
+                  if (isYp)
+                  {
+                      if (tongjiList.Any(s => s.GroupName == key))
+                      {
+                          tongjiList.Where(s => s.GroupName == key).FirstOrDefault().YPHL = PJHL;
+                      }
+                      else
+                      {
+                          tongjiList.Add(new Model.Tongji() { GroupName = key, YPHL = PJHL });
+                      }
+                  }
+                  else
+                  {
+                      if (tongjiList.Any(s => s.GroupName == key))
+                      {
+                          tongjiList.Where(s => s.GroupName == key).FirstOrDefault().TJHL = PJHL;
+                      }
+                      else
+                      {
+                          tongjiList.Add(new Model.Tongji() { GroupName = key, TJHL = PJHL });
+                      }
+                  }
+                  
+              };
+
+            Action<List<Model.DrugProjectItem>,bool> countPJHL = (List<Model.DrugProjectItem> models,bool fc) =>
             {
-                double PJHL = 0;
-                int lastId = 0;
-                double fangche = 0;
-                double lastHL = 0;
-                foreach(var sitem in item)
+                foreach (var item in models.GroupBy(s => s.CodeNum1))
                 {
-                    PJHL += Convert.ToDouble(sitem.HL);
-                    lastId = sitem.ID;
+                    double PJHL = 0;
+                    int lastId = 0;
+                    List<FcEntity> fclist = new List<Win.FcEntity>();
+                    int index = 0;
+                    foreach (var sitem in item)
+                    {
+                        PJHL += Convert.ToDouble(sitem.HL);
+                        lastId = sitem.ID;
+                        if (index > 1)
+                        {
+                            index = 0;
+                        }
+                        fclist.Add(new Win.FcEntity() { HL = Convert.ToDouble(sitem.HL), ID = sitem.ID });
+                    }
+                    if (lastId != 0 && item.Count() > 1)
+                    {
+                        PJHL = Math.Round(PJHL / item.Count(), config.PingJunHLPoint, MidpointRounding.AwayFromZero);
+                        list.Where(s => s.ID == lastId).FirstOrDefault().PJHL = PJHL.ToString();
+                        addTongji(item.Key, PJHL, fc);
+                        if (fc)
+                        {
+                            if (fclist.Count >= 2)
+                            {
+                                double fac = 0;
+                                fclist = fclist.OrderBy(s => s.ID).ToList();
+                                for (int i = 1; i < fclist.Count; i++)
+                                {
+                                    fac = fclist[i].HL - fclist[i - 1].HL;
+                                }
+                                list.Where(s => s.ID == lastId).FirstOrDefault().FC = fac.ToString();
+                            }
+                        }
+                    }
                 }
-                if (lastId != 0&&item.Count()>0)
-                {
-                    PJHL = Math.Round(PJHL / item.Count(),config.PingJunHLPoint,MidpointRounding.AwayFromZero);
-                    normallist.Where(s => s.ID == lastId).FirstOrDefault().PJHL = PJHL.ToString();
-                }
-            }
-            
-            foreach (var item in fucelist.GroupBy(s => s.CodeNum1))
-            {
-                double PJHL = 0;
-                foreach (var sitem in item)
-                {
-                    PJHL += Convert.ToDouble(sitem.HL);
-                }
-            }
+            };
+
+            countPJHL(yplist.Where(s => s.IsFuCe == "False").ToList(),true);
+            countPJHL(yplist.Where(s => s.IsFuCe != "False").ToList(),true);
+
+            countPJHL(tjlist.Where(s => s.IsFuCe == "False").ToList(),false);
+            countPJHL(tjlist.Where(s => s.IsFuCe != "False").ToList(),false);
+
             #endregion
 
             //统计数据
@@ -128,4 +175,13 @@ namespace JSCenter.Win
 
         #endregion
     }
+
+
+    public class FcEntity
+    {
+        public double HL { get; set; }
+
+        public int ID { get; set; }
+    }
+
 }
